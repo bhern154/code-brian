7c2ef12e2551d1b4346b1a0498dcf129ebfafff7
bhern154 <brian1010199@gmail.com>
2025-06-28 16:12:31 -0700

    updated navbar

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -17,19 +17,64 @@ $(document).ready(function() {
 function initializeNavbar() {
     console.log('Initializing navbar');
 
-    $('.toggle-btn').on('click', function() {
-        $('nav').toggleClass('collapsed');  // Toggle visibility of links
-        $(this).toggleClass('active');      // Change the arrow direction
+    $('.toggle-btn').on('click', function () {
+        const $nav = $('nav');
+        const $wrapper = $('.navbar-wrapper');
+        const isCollapsed = $nav.hasClass('collapsed');
+
+        $nav.toggleClass('collapsed');
+        $(this).toggleClass('active');
+
+        if (!isCollapsed) {
+            // COLLAPSING
+            $wrapper.addClass('collapsed');
+            $wrapper.addClass('animate-close');
+            void $wrapper[0].offsetWidth; // trigger reflow to restart animation
+            setTimeout(() => {
+                $wrapper.removeClass('animate-close');
+            }, 300); // match duration of bounce-shrink
+        } else {
+            // EXPANDING
+            $wrapper.removeClass('collapsed');
+            $wrapper.addClass('animate-open');
+            void $wrapper[0].offsetWidth; // trigger reflow to restart animation
+            setTimeout(() => {
+                $wrapper.removeClass('animate-open');
+            }, 300); // match duration of expand-smooth
+        }
     });
 }
 
+
 function setActiveNavbarItem() {
-    var currentPage = window.location.pathname; // Get the full path of the current URL
-    $('.navbar-wrapper nav a').each(function() {
-        var link = $(this).attr('href'); // Get the href attribute of each link
-        // Compare the current page URL with the link in the navbar
-        if (currentPage === link || currentPage.endsWith(link)) {
-            $(this).addClass('active-page');  // Add class to active item
+    const currentPage = window.location.pathname;
+    let $prevActive = $('.navbar-wrapper nav a.active-page .nav-icon');
+
+    $('.navbar-wrapper nav a').each(function () {
+        const link = $(this).attr('href');
+        const $icon = $(this).find('.nav-icon');
+        const isActive = (currentPage === link || currentPage.endsWith(link));
+
+        if (isActive) {
+            if (!$(this).hasClass('active-page')) {
+                // This is the NEW active item, fade icon change
+                $icon.addClass('fade');
+                setTimeout(() => {
+                    $icon.attr('src', $icon.data('icon-active'));
+                    $icon.removeClass('fade');
+                }, 150);
+            }
+            $(this).addClass('active-page');
+        } else {
+            if ($(this).hasClass('active-page')) {
+                // This was active but now not, fade back to outline icon
+                $icon.addClass('fade');
+                setTimeout(() => {
+                    $icon.attr('src', $icon.data('icon'));
+                    $icon.removeClass('fade');
+                }, 150);
+            }
+            $(this).removeClass('active-page');
         }
     });
 }
